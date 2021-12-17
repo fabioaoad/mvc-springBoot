5454ab3bbbc5c3ea7db679bda8702fff3aab1cc0
Fabio Martin Aoad <fabioaoad@gmail.com>
2021-12-17 11:54:07 -0300

    Thymeleaf
    Directiva if de Thymeleaf - S02A09

--- src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/web/app/controller/IndexController.java
@@ -1,5 +1,8 @@
 package com.bolsadeideas.springboot.web.app.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,10 +27,24 @@ public class IndexController {
 		Usuario usuario = new Usuario();
 		usuario.setNombre("Fabio Martin");
 		usuario.setApellido("Aoad");
+		usuario.setEmail("fabioaoad@gmail.com");
 		model.addAttribute("usuario", usuario);
 		model.addAttribute("titulo", "Perfil del Usuario: " .concat(usuario.getNombre()));
 		return "perfil";
 	}
+	
+	
+	
+	
+	@RequestMapping("/listar")
+	public String listar(Model model) {
+		List<Usuario> usuarios = new ArrayList<>();
+		model.addAttribute("titulo", "Listado de usuarios: ");
+		model.addAttribute("usuarios", usuarios);
+
+		return "listar";
+	}
+	
 	
 	
 }
